3216a5f2db2c91af00d9f3f3ac70d53173cf26ec
WangJieCS <hncs_wj@163.com>
2018-05-30 17:00:15 +0800

    make collide

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        int a;
+        int b;
+
         System.out.println("22222222");
         System.out.println("Hello World!");
         System.out.println("when i changed,and then  what will be happend!");
