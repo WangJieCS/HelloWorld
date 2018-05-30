78ee0ac06a6e545f1fccd0d8a83bad6285a60112
wangjie <hncs_wj@163.com>
2018-05-30 16:52:52 +0800

    test collide

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("make collide!");
+        System.out.println("22222222");
         System.out.println("Hello World!");
         System.out.println("when i changed,and then  what will be happend!");
         System.out.println("To test need or not need add!");
