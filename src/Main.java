72e78970d933684500f63e61136599c95381f2e1
WangJieCS <hncs_wj@163.com>
2018-05-30 17:12:06 +0800

    make collide

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int b;
         int c;
         int d;
-        System.out.println("111111111111");
+        System.out.println("111111111112");
         System.out.println("Hello World!");
         System.out.println("when i changed,and then  what will be happend!");
         System.out.println("To test need or not need add!");
